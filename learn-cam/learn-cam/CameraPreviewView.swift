51386d78051fd8d42d05413a96853860f5618216
taku-y <taku.yoshioka.4096@gmail.com>
2025-03-30 15:08:24 +0900

    Add circles to the camera image

--- learn-cam/learn-cam/CameraPreviewView.swift
+++ learn-cam/learn-cam/CameraPreviewView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import AVFoundation
+import UIKit
 
 struct CameraPreviewView: UIViewRepresentable {
     let previewLayer: AVCaptureVideoPreviewLayer
@@ -9,8 +10,14 @@ struct CameraPreviewView: UIViewRepresentable {
         view.backgroundColor = .clear
         view.previewLayer.session = previewLayer.session
         view.previewLayer.videoGravity = .resizeAspectFill
-        print("Initial preview layer frame: \(view.previewLayer.frame)")
-        print("Initial view bounds: \(view.bounds)")
+        
+        // オーバーレイビューの追加
+        let overlayView = CircleOverlayView()
+        overlayView.backgroundColor = .clear
+        view.addSubview(overlayView)
+        overlayView.frame = view.bounds
+        overlayView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
         return view
     }
     
@@ -49,5 +56,61 @@ class VideoView: UIView {
     
     var previewLayer: AVCaptureVideoPreviewLayer {
         layer as! AVCaptureVideoPreviewLayer
+    }
+}
+
+class CircleOverlayView: UIView {
+    private var circles: [(center: CGPoint, radius: CGFloat)] = []
+    private var displayLink: CADisplayLink?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupDisplayLink()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupDisplayLink()
+    }
+    
+    private func setupDisplayLink() {
+        displayLink = CADisplayLink(target: self, selector: #selector(updateCircles))
+        displayLink?.add(to: .main, forMode: .common)
+    }
+    
+    @objc private func updateCircles() {
+        // ランダムな円の生成
+        circles = (0..<5).map { _ in
+            let center = CGPoint(
+                x: CGFloat.random(in: 0...bounds.width),
+                y: CGFloat.random(in: 0...bounds.height)
+            )
+            let radius = CGFloat.random(in: 20...50)
+            return (center: center, radius: radius)
+        }
+        setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        for circle in circles {
+            context.setFillColor(UIColor.red.withAlphaComponent(0.3).cgColor)
+            context.setStrokeColor(UIColor.red.cgColor)
+            context.setLineWidth(2)
+            
+            context.addEllipse(in: CGRect(
+                x: circle.center.x - circle.radius,
+                y: circle.center.y - circle.radius,
+                width: circle.radius * 2,
+                height: circle.radius * 2
+            ))
+            
+            context.drawPath(using: .fillStroke)
+        }
+    }
+    
+    deinit {
+        displayLink?.invalidate()
     }
 } 
