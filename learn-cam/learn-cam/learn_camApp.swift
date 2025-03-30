e5794933609ec906248da34c186bd57e039104f4
taku-y <taku.yoshioka.4096@gmail.com>
2025-03-30 11:55:20 +0900

    Init xcode project
    Tweaks

---
+++ learn-cam/learn-cam/learn_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_camApp.swift
+//  learn-cam
+//
+//  Created by 吉岡琢 on 2025/03/30.
+//
+
+import SwiftUI
+
+@main
+struct learn_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learn-cam/learn-camTests/learn_camTests.swift
@@ -0,0 +1,17 @@
+//
+//  learn_camTests.swift
+//  learn-camTests
+//
+//  Created by 吉岡琢 on 2025/03/30.
+//
+
+import Testing
+@testable import learn_cam
+
+struct learn_camTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
